e57247b53a64e9b21d93a6b95d76abe48103664f
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-03-02 18:21:17 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+
+import mlflow
+import tempfile
+import os
+import wandb
+import hydra
+from omegaconf import DictConfig
+
+_steps = [
+    "download",
+    "basic_cleaning",
+    "data_check",
+    "data_split",
+    "train_random_forest",
+]
