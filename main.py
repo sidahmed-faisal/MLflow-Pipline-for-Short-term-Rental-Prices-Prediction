e9367ade0a9bb8e858c62e6bd7ac3f4640d09af9
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-03-10 02:08:49 +0400

    added data spliting step

--- main.py
+++ main.py
@@ -71,6 +71,18 @@ def go(config: DictConfig):
                     "max_price": config['etl']['max_price']
                 }
             )
+            
+        if "data_split" in active_steps:
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                }
+            )
 
 if __name__ == "__main__":
     go()
